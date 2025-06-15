f891c1c3af421cbbfb8b21c6d4bbb32c5470ec5c
Andresv27728 <andrescamilovallejogomez701@gmail.com>
2025-06-15 14:39:39 -0500

    Create Test.js

---
+++ plugins/Test.js
@@ -0,0 +1,63 @@
+let handler = async (m, { conn, usedPrefix: _p }) => { try { let name = await conn.getName(m.sender)
+
+let text = `‼️ ¡Hola! *@${m.sender.split('@')[0]}* soy *Makima ( OficialBot ).*
+
+╭━━I N F O-B O T━━ ┃Creador: 𓆩‌۫᷼ ִֶָღܾ݉͢ғ꯭ᴇ꯭፝ℓɪ꯭ͨא𓆪 ┃Tiempo activo: 00:01:26 ┃Baileys: Multi device. ┃Base: Oficial. ┃Registros: 63 ╰━━━━━━━━━━━━━
+
+.       ╭ֹ┈ ⵿❀⵿ ┈╮ ㅤ ╭ֹ┈ ⵿❀⵿ ┈╮INFO-USER ┃┈➤ Creador: Félix ┃┈➤ Cliente: ${name} ┃┈➤ Rango: Nuv ┃┈➤ Nivel: 0 ┃┈➤ País: Dominican Republic 🇩🇴 ╰━━━━━━━━━━━━━
+
+➪ 𝗟𝗜𝗦𝗧𝗔 ➪  𝗗𝗘 ➪ 𝗖𝗢𝗠𝗔𝗡𝗗𝗢𝗦
+
+.       ╭ֹ┈ ⵿❀⵿ ┈╮ ㅤ ╭ֹ┈ ⵿❀⵿ ┈╮PRINCIPALES ┃┈➤ #estado ┃┈➤ #botreglas ┃┈➤ #menu ┃┈➤ #menu2 ┃┈➤ #uptime ┃┈➤ #menulista ╰━━━━━━━━━━━━━
+
+.       ╭ֹ┈ ⵿❀⵿ ┈╮ ㅤ ╭ֹ┈ ⵿❀⵿ ┈╮BUSCADORES ┃┈➤ #gitthubsearch ┃┈➤ #google [Búsqueda] ┃┈➤ #tiktoksearch ┃┈➤ #pinterest ┃┈➤ #imagen [querry] ╰━━━━━━━━━━━━━━━━━━
+
+.       ╭ֹ┈ ⵿❀⵿ ┈╮ ㅤ ╭ֹ┈ ⵿❀⵿ ┈╮JUEGOS ┃┈➤ #abrazar ┃┈➤ #acertijo ┃┈➤ #agarrar ┃┈➤ #ahorcado ┃┈➤ #besar ┃┈➤ #acariciar ┃┈➤ #golpear ┃┈➤ #pregunta ┃┈➤ #reto ┃┈➤ #triste ┃┈➤ #reto ┃┈➤ #bot ┃┈➤ #love ┃┈➤ #consejo ┃┈➤ #dance ┃┈➤ #nombreninja ┃┈➤ #meme ┃┈➤ #dormir ┃┈➤ #rata ┃┈➤ #enamorada ┃┈➤ #gay ┃┈➤ #manco ┃┈➤ #apostar ┃┈➤ #piropo ┃┈➤ #sonrojarse ┃┈➤ #agarrar ╰━━━━━━━━━━━━━━━━━━
+
+.       ╭ֹ┈ ⵿❀⵿ ┈╮ ㅤ ╭ֹ┈ ⵿❀⵿ ┈╮WAIFU ┃┈➤ #robarpersonaje ┃┈➤ #obtenidos ┃┈➤ #sacar ┃┈➤ #guardar ┃┈➤ #carrw ┃┈➤ #confirmar ┃┈➤ #character ┃┈➤ #roll ┃┈➤ #top ╰━━━━━━━━━━━━━━━━━━
+
+.       ╭ֹ┈ ⵿❀⵿ ┈╮ ㅤ ╭ֹ┈ ⵿❀⵿ ┈╮REGISTROS ┃┈➤ #reg ┃┈➤ #unreg ┃┈➤ #profile ┃┈➤ #usuarios ╰━━━━━━━━━━━━━━━━━━
+
+.       ╭ֹ┈ ⵿❀⵿ ┈╮ ㅤ ╭ֹ┈ ⵿❀⵿ ┈╮ECONOMIA ┃┈➤ #daily ┃┈➤ #bank ┃┈➤ #robar ┃┈➤ #robarxp ┃┈➤ #rob2 ┃┈➤ #levelup ┃┈➤ #lb ┃┈➤ #mine ┃┈➤ #retirar ┃┈➤ #trabajar ┃┈➤ #transferir ╰━━━━━━━━━━━━━━━━━━
+
+.       ╭ֹ┈ ⵿❀⵿ ┈╮ ㅤ ╭ֹ┈ ⵿❀⵿ ┈╮DESCARGAS ┃┈➤ #fb ┃┈➤ #play ┃┈➤ #playvid ┃┈➤ #mediafire ┃┈➤ #apkmod ┃┈➤ #ytmp3doc ┃┈➤ #ytmp4doc ┃┈➤ #ig ┃┈➤ #gitclone ┃┈➤ #tiktok ┃┈➤ #spotify ┃┈➤ #tw ┃┈➤ #ytmp4 ┃┈➤ #imagen [querry] ╰━━━━━━━━━━━━━━━━━━
+
+.       ╭ֹ┈ ⵿❀⵿ ┈╮ ㅤ ╭ֹ┈ ⵿❀⵿ ┈╮GRUPOS ┃┈➤ #group abrir ┃┈➤ #group cerrar ┃┈➤ #delete ┃┈➤ #setppgroup ┃┈➤ #encuesta ┃┈➤ #rentar ┃┈➤ #kick ┃┈➤ #promote ┃┈➤ #demote ┃┈➤ #tagall ┃┈➤ #tag ┃┈➤ #invite ╰━━━━━━━━━━━━━━━━━━
+
+.       ╭ֹ┈ ⵿❀⵿ ┈╮ ㅤ ╭ֹ┈ ⵿❀⵿ ┈╮STICKERS ┃┈➤ #wm [autor] ┃┈➤ #s ┃┈➤ #qc ┃┈➤ #toimg ╰━━━━━━━━━━━━━━━━━━
+
+.       ╭ֹ┈ ⵿❀⵿ ┈╮ ㅤ ╭ֹ┈ ⵿❀⵿ ┈╮DATABASE ┃┈➤ #delvn ┃┈➤ #demsg ┃┈➤ #delimg ┃┈➤ #delsticker ┃┈➤ #infobot ╰━━━━━━━━━━━━━━━━━━
+
+.       ╭ֹ┈ ⵿❀⵿ ┈╮ ㅤ ╭ֹ┈ ⵿❀⵿ ┈╮EXPERIENCIA ┃┈➤ #buy ┃┈➤ #buyall ╰━━━━━━━━━━━━━━━━━━
+
+.       ╭ֹ┈ ⵿❀⵿ ┈╮ ㅤ ╭ֹ┈ ⵿❀⵿ ┈╮CONFIGURACIÓN ┃┈➤ #enable ┃┈➤ #disable ┃┈➤ #on ┃┈➤ #off ╰━━━━━━━━━━━━━━━━━
+
+.       ╭ֹ┈ ⵿❀⵿ ┈╮ ㅤ ╭ֹ┈ ⵿❀⵿ ┈╮ANIME ┃┈➤ #toanime ┃┈➤ #tts ┃┈➤ #remini ┃┈➤ #enhance ┃┈➤ #hd ┃┈➤ #nuevafotochannel ┃┈➤ #nosilenciarcanal ┃┈➤ #silenciarcanal ┃┈➤ #seguircanal ┃┈➤ #inspect ┃┈➤ #infobot ┃┈➤ #readvo ╰━━━━━━━━━━━━━━━━━━
+
+.       ╭ֹ┈ ⵿❀⵿ ┈╮ ㅤ ╭ֹ┈ ⵿❀⵿ ┈╮INFORMACIÓN ┃┈➤ #creador ┃┈➤ #owner ┃┈➤ #reportar ┃┈➤ #ping ┃┈➤ #links ╰━━━━━━━━━━━━━━━━━━
+
+.       ╭ֹ┈ ⵿❀⵿ ┈╮ ㅤ ╭ֹ┈ ⵿❀⵿ ┈╮CREADOR ┃┈➤ #addprem ┃┈➤ #copia ┃┈➤ #broadcastgroup ┃┈➤ #bcgb ┃┈➤ #bcgb2 ┃┈➤ #broadcast ┃┈➤ #bc ┃┈➤ #cheat ┃┈➤ #delprem ┃┈➤ #dsowner ┃┈➤ #fixmsgespera ┃┈➤ #get ┃┈➤ #prefix ┃┈➤ #reiniciar ┃┈➤ #saveplugin ┃┈➤ #update ┃┈➤ #resetpersonajes ╰━━━━━━━━━━━━━━━━━━
+
+.       ╭ֹ┈ ⵿❀⵿ ┈╮ ㅤ ╭ֹ┈ ⵿❀⵿ ┈╮DESARROLLADORES ┃┈➤ #autoadmin ┃┈➤ #banuser ┃┈➤ #unbanuser ┃┈➤ #banchat ┃┈➤ #unbanchat ┃┈➤ #ip ┃┈➤ #join ╰━━━━━━━━━━━━━━━━━━
+
+.       ╭ֹ┈ ⵿❀⵿ ┈╮ ㅤ ╭ֹ┈ ⵿❀⵿ ┈╮A - I ┃┈➤ #dalle ┃┈➤ #simi ┃┈➤ #ai ┃┈➤ #tovideo ┃┈➤ #togifaud ╰━━━━━━━━━━━━━━━━━━
+
+> © ⍴᥆ᥕᥱrᥱძ ᑲᥡ Félix Manuel`
+
+
+
+await conn.sendMessage(m.chat, {
+  image: { url: 'https://files.catbox.moe/0ro3o9.jpg' },
+  caption: text,
+  footer: '💙makima💎',
+  buttons: [
+    { buttonId: `${_p}grupos`, buttonText: { displayText: '🌐 ＧＲＵＰＯＳ' }, type: 1 },
+    { buttonId: `${_p}code`, buttonText: { displayText: '🕹 ＳＥＲＢＯＴ' }, type: 1 }
+  ],
+  viewOnce: true
+}, { quoted: m })
+
+} catch (e) { console.error(e) conn.reply(m.chat, '❎ Error al mostrar el menú.', m) } }
+
+handler.help = ['menutest'] handler.tags = ['main'] handler.command = ['menutest'] handler.register = true export default handler
+
