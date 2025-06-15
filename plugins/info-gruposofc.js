3bc0df6c7d5a14fae7ae7e966c31a8b96e49d541
mantis-has <mantisbotmd@gmail.com>
2025-06-15 16:38:59 -0400

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -3,25 +3,23 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn }) => {
 
   const namegrupo = 'Grupo Oficial'
-  const gp1 = 'https://chat.whatsapp.com/nohay' // ← tu link real
+  const gp1 = 'https://chat.whatsapp.com/KoJjHo6o3Ew7P5qkjaIh0r' // ← tu link real
 
-  const namechannel = 'Canal del Bot'
-  const channel = 'https://whatsapp.com/channel/no hay' // ← tu canal real
+  const namechannel = 'Canal de la Bot'
+  const channel = 'https://whatsapp.com/channel/0029VbAZcyIIXnlwp79iwu2l' // ← tu canal real
 
-  const dev = '👾 Desarrollador: @thecarlos19'
-  const catalogo = 'https://qu.ax/TJRoN.jpg' // o './media/grupos.jpg'
-  const emojis = '📡'
+  const dev = '💎 Creador: Félix Manuel'
+  const catalogo = 'https://qu.ax/dXOUo.jpg' // o './media/grupos.jpg'
+  const emojis = '👨‍💻'
 
   let grupos = `
-╭─⟪ *🌐 GRUPOS OFICIALES * 
+╭─⟪ *💎GRUPOS OFICIALES * 
 │
-│ ⚔️ *${namegrupo}*
+│ 🩵 *${namegrupo}*
 │ ${gp1}
 │
-│ ⚡ *${namechannel}*
+│ 🩵 *${namechannel}*
 │ ${channel}
-│
-│ ${dev}
 ╰─────────────────╯
 `
 
