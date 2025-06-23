27057e577590a6cf4a889bffb6a1c83aa6eb5b8d
mantis-has <mantisbotmd@gmail.com>
2025-06-23 16:09:38 -0400

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -1,19 +1,19 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn }) => {
-  let txt_owner = "¡Hola! Este es el Contacto de mi creador\n\n> https://wa.me/18293142989?text=Hola+Félix"
+  let txt_owner = "¡Hola! Mi creadora no permite mostrar su número de telefono."
   try {
     let res = await fetch("https://qu.ax/dXOUo.jpg")
     let buffer = await res.buffer()
     await conn.sendFile(m.chat, buffer, 'thumbnail.jpg', txt_owner, m)
   } catch (e) {
     console.error(e)
-    m.reply('👨‍💻 No se pudo enviar la imagen del creador. Intenta más tarde...')
+    m.reply('👨‍💻 No se pudo enviar la imagen del bot. Intenta más tarde...')
   }
 }
 
 handler.help = ['owner']
 handler.tags = ['main']
-handler.command = ['owner', 'creator', 'creador', 'dueño']
+handler.command = ['owner', 'creatora', 'creadora', 'dueña']
 
 export default handler
