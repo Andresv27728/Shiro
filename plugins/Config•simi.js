9af1404d5ff1abf0591fe838f6340002af7eae80
mantis-has <mantisbotmd@gmail.com>
2025-06-24 07:35:18 -0400

    Actualizar Config•simi.js

--- plugins/Config•simi.js
+++ plugins/Config•simi.js
@@ -14,7 +14,7 @@ handler.before = async (m) => {
      // await m.conn.sendMessage(m.chat, { text: ressimi.resultado.simsimi }, { quoted: m });
         await conn.reply(m.chat, ressimi.resultado.simsimi, m)
     } catch {
-      throw '🍟 *Ocurrió un error*';
+      throw '🩵 *Ocurrió un error...*';
     }
     return !0;
   }
