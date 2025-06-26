b56f80980b369ce294cc5e6cd81471db50570305
mantis-has <mantisbotmd@gmail.com>
2025-06-26 08:26:57 -0400

    Actualizar Owner•nuevonombrebot.js

--- plugins/Owner•nuevonombrebot.js
+++ plugins/Owner•nuevonombrebot.js
@@ -1,31 +1,17 @@
-let handler = async (m, { conn, text, isOwner }) => {
-  let botNumber = (conn.user && conn.user.id) ? conn.user.id.split('@')[0] : ''
-  let senderNumber = m.sender.split('@')[0]
-
-  if (!isOwner && senderNumber !== botNumber) {
-    return conn.reply(m.chat, '「🩵」Este comando solo puede ser utilizado por el owner o el propio bot.', m)
-  }
-
-  if (!text) return conn.reply(m.chat, `🩵 *¿Qué nombre deseas ponerme?*`, m)
-
-  if (typeof conn.updateProfileName !== 'function') {
-    return conn.reply(m.chat, '🚩 Este bot no soporta cambiar el nombre por este método.', m)
-  }
-
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  if (!text) throw '𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉 𝙉𝙊𝙈𝘽𝙍𝙀'
   try {
     await conn.updateProfileName(text)
-    await conn.reply(m.chat, '✅️ *Nombre cambiado con éxito*', m)
-    await m.react('✅')
+    m.reply('LISTO!')
   } catch (e) {
     console.log(e)
-    await m.react('❌')
-    await conn.reply(m.chat, `🩵 ¡Ocurrió un error!\n${e}`, m)
+    throw `Error`
   }
 }
-
-handler.help = ['nuevonombrebot <nombre>']
+handler.help = ['setbotname <nombre>']
 handler.tags = ['owner']
-handler.command = ['nuevonombrebot', 'setname', 'namebot']
-handler.owner = false
+handler.command = /^(setbotname)$/i
+
+handler.owner = true
 
 export default handler
