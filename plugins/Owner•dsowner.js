0ff018a1ca7131bc9b646f2c2195b95b12a4b20a
mantis-has <mantisbotmd@gmail.com>
2025-06-26 07:47:30 -0400

    Actualizar Owner•dsowner.js

--- plugins/Owner•dsowner.js
+++ plugins/Owner•dsowner.js
@@ -31,7 +31,7 @@ await conn.reply(m.chat, '🩵 *La carpeta esta vacía*',  m, rcanal, )
 } else {
 m.react(done)
 await conn.reply(m.chat, `🩵 *Se eliminaron ${filesDeleted} archivos de sesión, excepto el archivo creds.json*`,  m, rcanal, )
-conn.reply(m.chat, `🩵 *¡Hola! Soy Makima*`, m, rcanal, )
+conn.reply(m.chat, `🩵 *¡Hola! Soy ${botname}*`, m, rcanal, )
 
 }
 } catch (err) {
