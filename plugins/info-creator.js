519e16a2227b751c7c5c710d82644526ec19c130
Andresv27728 <andrescamilovallejogomez701@gmail.com>
2025-07-05 01:34:58 -0500

    Update info-creator.js

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -1,15 +1,15 @@
 let handler = async (m, { conn }) => {
-  // Reacciona con 💎
+  // Reacciona con 👑
   if (conn.sendMessage) {
     await conn.sendMessage(m.chat, { react: { text: '💎', key: m.key }});
   }
 
   // Datos de los contactos
-  let numberCreator = '18293142989' // Número de la creadora
-  let nombreCreator = '💎 C R E A D O R 💎'
-  let canal = 'https://wa.me18293142989'
+  let numberCreator = '573133374132' // Número del creador
+  let nombreCreator = ' C R E A D O R '
+  let canal = 'https://chat.whatsapp.com/JkpwB3J7qMQF1uxomv5U1e' 
 
-  let numberBot = '527222518356' // Número del bot
+  let numberBot = 'no hay' // Número del bot
   let nombreBot = 'BOT OFICIAL'
 
   let numberYoSoyYo = '573133374132'
@@ -38,7 +38,7 @@ TEL;waid=${numberYoSoyYo}:${numberYoSoyYo}
 END:VCARD`
 
   // Envía el canal como texto
-  await conn.sendMessage(m.chat, { text: `💙 AQUI ESTA EL NUMERO DE MI CREADOR Y MÁS CONTACTOS` }, { quoted: m })
+  await conn.sendMessage(m.chat, { text: `🖤 AQUI ESTA EL NUMERO DE MI CREADOR Y MÁS CONTACTOS` }, { quoted: m })
 
   // Envía la tarjeta de contacto con los tres contactos
   await conn.sendMessage(m.chat, {
